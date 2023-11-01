2be9513c7abd4c46fb78ea186192c28b2a8025ef
rindra daniel <rindra.randriambololona@gmail.com>
2023-11-02 00:57:05 +0100

    refactor

--- src/main/kotlin/RomanNumeralsConverter.kt
+++ src/main/kotlin/RomanNumeralsConverter.kt
@@ -4,12 +4,9 @@ class RomanNumeralsConverter {
             number == 4 -> "IV"
             number == 9 -> "IX"
             number == 14 -> "XIV"
-            number == 15 -> "XV"
-            number == 16 -> "XVI"
-            number == 17 -> "XVII"
-            number == 18 -> "XVIII"
-            number >= 10 -> "X" + "I".repeat(number-10)
-            number >= 5 -> "V" + "I".repeat(number-5)
+            number >= 15 -> "X" + "V" + "I".repeat(number - 15)
+            number >= 10 -> "X" + "I".repeat(number - 10)
+            number >= 5 -> "V" + "I".repeat(number - 5)
             else -> "I".repeat(number)
         }
 }
