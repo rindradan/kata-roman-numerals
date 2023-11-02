fb0a6abee393d053a8a36a785a356c9e59a0cdbb
rindra daniel <rindra.randriambololona@gmail.com>
2023-11-02 02:12:31 +0100

    refactor

--- src/main/kotlin/RomanNumeralsConverter.kt
+++ src/main/kotlin/RomanNumeralsConverter.kt
@@ -1,5 +1,13 @@
 class RomanNumeralsConverter {
 
+    private companion object {
+        const val I = "I"
+        const val V = "V"
+        const val X = "X"
+        const val L = "L"
+        const val EMPTY = ""
+    }
+
     private val exceptions = mapOf(
         4 to "IV",
         9 to "IX",
@@ -15,13 +23,13 @@ class RomanNumeralsConverter {
 
     private fun Int.transformDecimals() : String =
         when {
-            (this - 50) >= 0 -> "L" + (this - 50).transformDecimals()
-            (this - 10) >= 0 -> "X" + (this - 10).transformDecimals()
-            (this - 5) >= 0 -> "V" + (this - 5).transformDecimals()
-            else -> ""
+            (this - 50) >= 0 -> L + (this - 50).transformDecimals()
+            (this - 10) >= 0 -> X + (this - 10).transformDecimals()
+            (this -  5) >= 0 -> V + (this -  5).transformDecimals()
+            else -> EMPTY
         }
 
-    private fun Int.transformUnits() = "I".repeat(this % 5)
+    private fun Int.transformUnits() = I.repeat(this % 5)
 
     private fun isException(number: Int) : Boolean = exceptions.containsKey(number)
 }
